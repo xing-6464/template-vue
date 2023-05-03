6b60ac90820a31c1d0458043e3eb1c043e9e2ef0
xing-6464 <xing_6464@foxmail.com>
2023-05-04 03:40:45 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/login',
+      component: () => import('@/views/login/Login.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/login/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="login-container">
+    <el-form class="login-form">
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
+      <!-- username -->
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <Avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="username" name="username" type="text"></el-input>
+      </el-form-item>
+      <!-- password -->
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <Avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="password" name="password"></el-input>
+        <span class="svg-pwd">
+          <el-icon>
+            <Avatar />
+          </el-icon>
+        </span>
+      </el-form-item>
+
+      <!-- 登录按钮 -->
+      <el-button style="width: 100%; margin-bottom: 30px;" type="primary">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Avatar } from '@element-plus/icons-vue'
+
+</script>
+
+<style lang="scss" scoped></style>
