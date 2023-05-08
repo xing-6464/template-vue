a365ceb69759634c1ec675e98c406664ee6b8de6
xing-6464 <xing_6464@foxmail.com>
2023-05-08 15:09:17 +0800

    样式创建

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="">
-    layout 页面
+  <div class="app-wrapper">
+    <!-- 左侧 -->
+    <SideBar class="sidebar-container"></SideBar>
+    <div class="main-container">
+      <div class="fixed-header">
+        <NavBar />
+      </div>
+      <!-- 内容区 -->
+      <AppMain></AppMain>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import NavBar from './components/NavBar.vue'
+import AppMain from './components/AppMain.vue'
+import SideBar from './components/SideBar/index.vue'
 
 </script>
 
