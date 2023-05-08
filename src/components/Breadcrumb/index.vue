62c5b1863207ec68b5ae41adaab94ee474ca23b5
xing-6464 <xing_6464@foxmail.com>
2023-05-09 03:00:05 +0800

    动态面包屑

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,15 +1,42 @@
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-    <el-breadcrumb-item>
-      <span class="no-redirect">活动详情</span>
+    <el-breadcrumb-item v-for="(item, index) in breadcrumbData" :key="item.path">
+      <!-- 不可点击 -->
+      <span class="no-redirect" v-if="index === breadcrumbData.length - 1">{{ item.meta.title }}</span>
+      <!-- 可以点击 -->
+      <span class="redirect" v-else @click="onLinkClick(item)">{{ item.meta.title }}</span>
+
     </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script lang='ts' setup>
+import { ref, watch } from 'vue'
+import { useRoute, type RouteLocationMatched, useRouter } from 'vue-router';
+import { useGetters } from '../../stores/getter';
+import { storeToRefs } from 'pinia';
+
+const breadcrumbData = ref<Array<RouteLocationMatched>>([])
+const getBreadcrumbData = () => {
+  breadcrumbData.value = route.matched.filter(item => item.meta && item.meta.title)
+}
+const getters = useGetters()
+const { styleVal } = storeToRefs(getters)
+const linkHoverColor = ref(styleVal.value.menuBg)
+
+// 监听 路由变化
+const route = useRoute()
+watch(route, () => {
+  getBreadcrumbData()
+}, {
+  immediate: true
+})
+
+const router = useRouter()
+function onLinkClick(item: RouteLocationMatched) {
+  router.push(item.path)
+}
+
 
 </script>
 
@@ -19,6 +46,16 @@
   font-size: 14px;
   line-height: 50px;
   margin-left: 30px;
+
+  .redirect {
+    color: #666;
+    font-weight: 600;
+    cursor: pointer;
+  }
+
+  .redirect:hover {
+    color: v-bind(linkHoverColor);
+  }
 
   ::v-deep .no-redirect {
     color: #97a8be;
